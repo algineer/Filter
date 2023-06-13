222fc848bcdba6c1f7bb00c0beb731fe83454979
algineer <120684984+algineer@users.noreply.github.com>
2023-06-13 10:22:59 -0400

    Update filter.js
    Add TamperMonkey Settings

--- filter.js
+++ filter.js
@@ -1,47 +1,64 @@
-var set_up = true
-const filter = document.createElement('textarea')
+// ==UserScript==
+// @name         Filter
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://flide.ap.tesla.services/3d/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tesla.services
+// @grant        none
+// ==/UserScript==
 
-var filter_once = false
+(function() {
+    //'use strict';
+
+    //--------------Global_FLIDE_CLIPS--------------
+
+    var set_up = true
+    const filter = document.createElement('textarea')
+
+    var filter_once = false
 
 
 
-//Comment Filter
-function Filter() {
-    let comments = document.querySelector("div.css-181veuo > select")
-    if (comments != null) {
+    //Comment Filter
+    function Filter() {
+        let comments = document.querySelector("div.css-181veuo > select")
+        if (comments != null) {
 
-        if (!filter_once) {
-            comments.parentElement.insertBefore(filter, comments)
-            filter_once = true
-            filter.focus()
-        }
+            if (!filter_once) {
+                comments.parentElement.insertBefore(filter, comments)
+                filter_once = true
+                filter.focus()
+            }
 
-        comments.childNodes.forEach(
-            function(comment) {
-                if (filter.value == '')
-                    comment.style.display = ''
-                else {
-                    if (!comment.value.toLowerCase().includes(filter.value.toLowerCase().trim()))
-                        comment.style.display = 'none'
-                    else
+            comments.childNodes.forEach(
+                function(comment) {
+                    if (filter.value == '')
                         comment.style.display = ''
-                }
-            })
+                    else {
+                        if (!comment.value.toLowerCase().includes(filter.value.toLowerCase().trim()))
+                            comment.style.display = 'none'
+                        else
+                            comment.style.display = ''
+                    }
+                })
 
-    } else {
-        filter_once = false
-        filter.value = ''
+        } else {
+            filter_once = false
+            filter.value = ''
+        }
+    };
+
+
+
+    if (window.location.href.includes("MESH"))
+        requestAnimationFrame(clip_loop);
+
+    function clip_loop() {
+
+        Filter()
+
+        requestAnimationFrame(clip_loop)
     }
-};
-
-
-
-if (window.location.href.includes("MESH"))
-    requestAnimationFrame(clip_loop);
-
-function clip_loop() {
-
-    Filter()
-
-    requestAnimationFrame(clip_loop)
-}
+})();
